f633471e42fa17f89f97c528cbfc123a6b271a80
getty <gcherono15@gmail.com>
2021-07-19 08:38:15 +0300

    created angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
+import { QuotesComponent } from './quotes/quotes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    DetailsComponent,
+    QuotesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
